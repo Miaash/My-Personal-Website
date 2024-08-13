0ef9acdeb80231fb5f17a6555cb20c035870f1db
seongahong <girl0930h@gmail.com>
2024-08-14 00:13:40 +0900

    [수정] tailwind css 적용 test

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,3 +1,10 @@
 export default function MainPage() {
-  return <div>my personal website</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">My Personal Website</h1>
+      {/* <button className="btn mr-2 mb-2 btn-primary" type="button">
+        <span className="btn-text">Primary</span>
+      </button> */}
+    </div>
+  );
 }
