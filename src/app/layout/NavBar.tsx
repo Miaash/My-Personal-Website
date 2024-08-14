bbca529a76176c005ee9aeaea431a0ecdc6f567f
seongahong <girl0930h@gmail.com>
2024-08-14 15:35:50 +0900

    [추가] 네비게이션 바 퍼블리싱 완료

---
+++ src/app/layout/NavBar.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+/**
+ * [네비게이션 바 컴포넌트]
+ * - 현재 띄워져있는 팝업창 관리.
+ * - 시계.
+ * @returns
+ */
+export default function NavBar() {
+  // TODO 시계 기능 구현 -> use client?
+  // 네비게이션바 시계
+  //   const [time, setTime] = useState("");
+  //   useEffect(() => {
+  //     setTime("12:38");
+  //   }, []);
+  const openedPopup = [
+    {
+      id: 1,
+      title: "first popup",
+      path: "/main",
+    },
+    {
+      id: 2,
+      title: "second popup",
+      path: "/main",
+    },
+    {
+      id: 3,
+      title: "third popup",
+      path: "/main",
+    },
+  ];
+  return (
+    <footer className="fixed bottom-0 left-0 w-full">
+      <nav
+        id="navbar-footer"
+        className="navbar navbar-main navbar-expand-lg navbar-dark justify-content-between navbar-footer flex items-center justify-between"
+      >
+        <ul className="navbar-nav navbar-nav-hover align-items-center flex flex-row items-center">
+          <li className="nav-item">
+            <a href="/main" className="nav-link" role="button">
+              <span className="w95-start inline-block h-16 w-16"></span>
+              <span className="nav-link-inner-text">Start</span>
+            </a>
+          </li>
+          {openedPopup.map((popup) => (
+            <li className="nav-item" key={popup.id}>
+              <a href="/main" className="nav-link" role="button">
+                <span className="nav-link-inner-text">{popup.title}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="time text-center">1:47 PM</div>
+      </nav>
+    </footer>
+  );
+}
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,10 +1,13 @@
+import NavBar from "../layout/NavBar";
+
 export default function MainPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">My Personal Website</h1>
-      {/* <button className="btn mr-2 mb-2 btn-primary" type="button">
+      {/* <p className="text font-bold underline">My Personal Website</p> */}
+      {/* <button className="btn btn-sm btn-primary border-dark mr-2" type="button">
         <span className="btn-text">Primary</span>
       </button> */}
+      <span className="w95-opened-file"></span>
     </div>
   );
 }
