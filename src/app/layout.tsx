4105526a123838e8efe4b92fd0787c88a0f6da0b
seongahong <girl0930h@gmail.com>
2024-09-09 17:59:29 +0900

    [update] 정적 metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,15 @@
 import "./globals.css";
 import "../styles/pixel.css";
 import NavBar from "./layout/NavBar";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MiaSoft99",
+  description: "hongseonga's website",
+  icons: {
+    icon: "/images/w95-icons-windows.png",
+  },
+};
 
 export default function RootLayout({
   children,
